6b804e325be4f7abb737cf1f46e632b4ee5ffb19
Joy Lal Chattaraj <joyc@qubole.com>
2019-12-20 11:50:06 +0530

    Fix Start Command

--- hive-metastore/entrypoint.sh
+++ hive-metastore/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
 envsubst < /opt/hadoop/etc/hadoop/hive-site.xml.tpl > /opt/hadoop/etc/hadoop/hive-site.xml
 
-exec "$@"
+hive --service metastore
